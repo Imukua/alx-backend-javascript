871741515a8ce2e1c2ab25302c5ed1cfa2471760
Imukua <ianmukuaa@gmail.com>
2023-11-28 00:50:58 +0300

    rewrote functions

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,27 +1,8 @@
 export default function createIteratorObject(report) {
-  const departments = Object.keys(report.allEmployees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
+  let result = [];
+  for (const value of Object.values(report.allEmployees)) {
+    result = [...result, ...value];
+  }
 
-  return {
-    next() {
-      if (currentDepartmentIndex < departments.length) {
-        const currentDepartment = departments[currentDepartmentIndex];
-        const employees = report.allEmployees[currentDepartment];
-
-        if (currentEmployeeIndex < employees.length) {
-          return { value: employees[currentEmployeeIndex++], done: false };
-        } else {
-          currentDepartmentIndex++;
-          currentEmployeeIndex = 0;
-          return this.next();
-        }
-      } else {
-        return { done: true };
-      }
-    },
-    [Symbol.iterator]() {
-      return this;
-    }
-  };
+  return result;
 }
