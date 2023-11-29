302b9bfd3f7748c199d4f5b4e0d8cd7a5e172cd3
Imukua <ianmukuaa@gmail.com>
2023-11-29 23:42:08 +0300

    eslint fix, again 🙄

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,4 @@
-export default function divideFunction (numerator, denominitor) {
+export default function divideFunction(numerator, denominitor) {
   if (denominitor === 0) {
     throw Error('cannot divide by 0');
   } else {
