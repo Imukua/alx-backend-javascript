1c2b128b5489588b1c8e06194aa71018984e6cb2
Imukua <ianmukuaa@gmail.com>
2023-11-30 19:33:33 +0300

    implemented task 2 🤞

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (!Array.isArray(students)) {
+      throw new Error('Students is not an array');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(Newname) {
+    if (typeof name !== 'string') {
+      throw new Error('name is not a string');
+    }
+    this._name = Newname;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(Newlength) {
+    if (typeof length !== 'number') {
+      throw new Error('length is not a number');
+    }
+    this._length = Newlength;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new Error('Students is not an array');
+    }
+    this._students = newStudents;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
