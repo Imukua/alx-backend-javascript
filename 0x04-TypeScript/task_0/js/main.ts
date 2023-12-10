525935940ea25e88cea636de395ab4d01605b2dc
Imukua <ianmukuaa@gmail.com>
2023-12-10 18:37:02 +0300

    Done like Ombaachi👨‍🍳

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,37 @@
+//  the Student interface
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+// student objects
+const firstStudent: Student = {
+  firstName: "Obed",
+  lastName: "Ehoneah",
+  age: 97,
+  location: "Accra"
+};
+
+const secondStudent: Student = {
+  firstName: "Frank",
+  lastName: "Ehoneah",
+  age: 79,
+  location: "Kumasi"
+};
+
+// array of students
+const studentsList = [firstStudent, secondStudent];
+
+const table = document.getElementById("studentTable") as HTMLTableElement;
+
+//new row for each student and append it to the table
+studentsList.forEach((student) => {
+  const row = table.insertRow();
+  const firstNameCell = row.insertCell(0);
+  const locationCell = row.insertCell(1);
+
+  firstNameCell.innerHTML = student.firstName;
+  locationCell.innerHTML = student.location;
+});
